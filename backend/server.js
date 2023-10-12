f6752a8fdff5cf6fa3d32e724bb0be4ea4aeb25a
Trevor-McGuire <121839229+Trevor-McGuire@users.noreply.github.com>
2023-10-12 17:26:51 -0500

    changed connection method

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
-const { getUserId } = require('./utils/auth')
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const { authMiddleware } = require("./utils/auth");
+const { getUserId } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,33 +15,39 @@ const server = new ApolloServer({
   resolvers,
 });
 
-
 const startApolloServer = async () => {
   await server.start();
-  
+
   const configureMiddleware = () => {
     console.log("Configuring middleware...");
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
-    app.use('/graphql', expressMiddleware(server, {
-      context: authMiddleware
-    }));
+    app.use(
+      "/graphql",
+      expressMiddleware(server, {
+        context: authMiddleware,
+      })
+    );
     console.log("Middleware configured.");
   };
-  
+
   configureMiddleware();
-  
+
   // if we're in production, serve client/dist as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  if (process.env.NODE_ENV === "production") {
+    // Use the absolute path from the root directory
+    const clientDistPath = path.join(__dirname, "client/dist");
+
+    app.use(express.static(clientDistPath));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(clientDistPath, "index.html"));
     });
-  } 
-  
-  if (typeof db.on === 'function') {
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
+  }
+
+  if (typeof db.on === "function") {
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", () => {
       app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
