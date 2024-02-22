33a0844edceba6be60ab9d48e8f180ac0f9ec053
Trevor-McGuire <121839229+Trevor-McGuire@users.noreply.github.com>
2024-02-22 15:09:30 -0600

    Refactor Home component and add eBay OAuth functionality. set up a secure redirect url though ngrok.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,46 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
-import ProductList from "../../components/ProductList/ProductList";
-import HeaderImage from "./HeaderImage";
-import { READ_PRODUCTS } from "../../utils/queries";
+import React, { useEffect } from "react";
+import EbaySocial from "../../components/OauthLogIn/ebay";
+import { gql } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+
+const EXCHANGE_AUTHORIZATION_CODE = gql`
+  mutation ExchangeAuthorizationCode($code: String!) {
+    exchangeAuthorizationCode(code: $code) {
+      access_token
+    }
+  }
+`;
 
 const Home = () => {
-  const { data } = useQuery(READ_PRODUCTS);
-  const home = data?.readProducts || null;
+
+  // wait for param code in url and make a request to the backend to exchange it for an access token
+  const [exchangeAuthorizationCode] = useMutation(EXCHANGE_AUTHORIZATION_CODE);
+  useEffect(() => {
+    const handleAuthorizationCode = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const authorizationCode = urlParams.get("code");
+      if (authorizationCode) {
+        try {
+          const { data } = await exchangeAuthorizationCode({
+            variables: { code: authorizationCode },
+          });
+          const accessToken = data.exchangeAuthorizationCode.access_token;
+          localStorage.setItem("access_token", accessToken);
+        } catch (error) {
+          console.error(
+            "Error exchanging authorization code for access token",
+            error
+          );
+        }
+      }
+    };
+    handleAuthorizationCode();
+  }, [exchangeAuthorizationCode]);
 
   return (
     <>
-      
-
+      <h1>Home</h1>
+      <EbaySocial />
     </>
   );
 };
