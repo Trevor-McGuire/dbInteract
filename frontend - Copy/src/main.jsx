08ad02deb8e0e761cce866c4fa858c7657f509ac
Trevor-McGuire <121839229+Trevor-McGuire@users.noreply.github.com>
2024-06-23 16:49:51 -0500

    Refactor frontend code and remove unused files

--- frontend - Copy/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
